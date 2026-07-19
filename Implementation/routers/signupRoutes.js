166a0eb285946605b04b4925722e504644bf61da
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-07-19 23:58:17 +0530

    divya

---
+++ Implementation/routers/signupRoutes.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const user=require('../models/model.js');
+const router=express.Router();
+
+
+router.post('/signup',async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+
+        await user.create({email,password});
+
+        res.send("Signup Successfully");
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).send("Error signing up");
+    }
+});
+
+
+module.exports=router;
